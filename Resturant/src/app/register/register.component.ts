a6944776c7fb7011afe80119b9b330598a9a7e6b
PR31 <pranavchp2431@gmail.com>
2021-05-26 15:58:02 +0530

    first commit

---
+++ Resturant/src/app/register/register.component.ts
@@ -0,0 +1,37 @@
+import { RestoService } from './../resto.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  alert:boolean=false
+
+  register = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  constructor(private resto:RestoService) { }
+
+  ngOnInit(): void {
+  }
+  collection(){
+    console.warn(this.register.value)
+    this.resto.registerUser(this.register.value).subscribe((result)=>{
+      console.warn(result);
+      this.alert=true
+    })
+  }
+  closerAlert()
+  {
+    this.alert=false
+  }
+
+}
